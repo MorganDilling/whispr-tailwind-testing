98ffeaa28ffb2dde61843be42d3aeef6455d348b
Morgan Dilling <morgandilling171@gmail.com>
2024-04-28 23:32:52 +0100

    fix: Fix for tooltips and scroll

--- src/lib/components/TooltipContainer.svelte
+++ src/lib/components/TooltipContainer.svelte
@@ -15,7 +15,7 @@
 		<div
 			bind:this={tooltipEl}
 			class="absolute whitespace-nowrap shadow-background-950 shadow-md pl-2 pr-2 pt-1 pb-1 bg-background-925 text-text-100 rounded-lg after:absolute after:top-[calc(100%-1px)] after:left-1/2 after:-ml-2 after:border-8 after:border-t-background-925 after:border-b-transparent after:border-l-transparent after:border-r-transparent"
-			style={`top: ${position.y - 45}px; left: ${position.x - tooltipEl?.getBoundingClientRect().width / 2}px;`}
+			style={`top: ${position.y - 45 + window.scrollY}px; left: ${position.x - tooltipEl?.getBoundingClientRect().width / 2 + window.scrollX}px;`}
 		>
 			{tooltip}
 		</div>
